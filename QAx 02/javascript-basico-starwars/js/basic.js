df1b12f7ca599156d098ff88d85921d20b36c13c
WELLINGTON IVAN RODRIGUES DA SILVA <94490227+Tim-Wellington@users.noreply.github.com>
2024-03-23 14:49:56 -0300

    Curso Java script

---
+++ QAx 02/javascript-basico-starwars/js/basic.js
@@ -0,0 +1,66 @@
+console.log('olá, javascript!')
+
+var userName = 'Thomas Rodrigues da Silva'
+
+document.getElementById('user-name').innerHTML = userName
+
+
+// variaves //
+
+
+
+// var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//Operadores matematicos //
+
+// var n1 = 15
+// var n2 = 5
+
+// console.log(typeof n1)
+// console.log(typeof n2)
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para dividir 
+
+// var total = n1 + n2
+// console.log(total)
+
+// operadores de comparação //
+
+// var v1 = 5
+// var v2 = 5
+
+// var resultado = v1 === v2
+// console.log(resultado)
+
+// Funções //
+
+//function soma(n1, n2) {
+//    console.log(n1, n2)
+// }
+
+// soma(5, 100)
+
+// function boasVindas(nome){
+//    alert(nome + ', seja bem-vindo(a)')
+// }
+
+//boasVindas('Thomas')
+
+//function soma(ni, n2){
+//    return n1 + n2
+// }
+
+// var resultado = soma(5,5)
+// console.log(resultado)
+
+// Controle de fluxos //
+
+// sendo um cliente correntista do banco 
+// Posso  sacar dinheiro em caixas eletrônicos
+// Para poder comprara em lugares que não aceitam o cartão de débito ou crédito 
+
